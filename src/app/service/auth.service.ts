c36a8d18903016d08e4554945b7c09238caa9c37
Gonzales Walisson <walisson.gonzales@gmail.com>
2021-06-30 19:53:23 -0300

    Pagina inicio

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -13,10 +14,23 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  login(userLogin: UserLogin): Observable<UserLogin>{
-      return this.http.post<UserLogin>('https://pessoalbloggonzales.herokuapp.com/usuarios/logar', userLogin)
+  login(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('https://pessoalbloggonzales.herokuapp.com/usuarios/logar', userLogin)
   }
-  cadastrar( user: User): Observable<User>{
-    return this.http.post<User>('https://pessoalbloggonzales.herokuapp.com/usuarios/cadastrar', user)
+
+
+
+cadastrar(user: User): Observable < User > {
+  return this.http.post<User>('https://pessoalbloggonzales.herokuapp.com/usuarios/cadastrar', user)
+}
+
+logado(){
+  let ok: boolean = false
+
+  if (environment.token != '') {
+    ok = true
   }
+
+  return ok
+}
 }
