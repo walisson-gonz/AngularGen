5b5669317da7e783c69d156a53ae14d7625930ad
Gonzales Walisson <walisson.gonzales@gmail.com>
2021-07-13 09:11:23 -0300

    Implementação do put e delete de postagem

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -20,7 +20,19 @@ export class PostagemService {
       return this.http.get<Postagem[]>('https://pessoalbloggonzales.herokuapp.com/postagens', this.token)
     }
 
+    getByIdPostagem(id:number): Observable<Postagem>{
+      return this.http.get<Postagem>(`https://pessoalbloggonzales.herokuapp.com/postagens/${id}`, this.token)
+    }
+
     postPostagem(postagem: Postagem): Observable<Postagem>{
       return this.http.post<Postagem>('https://pessoalbloggonzales.herokuapp.com/postagens', postagem, this.token)
+    }
+
+    putPostagem(postagem: Postagem): Observable<Postagem>{
+      return this.http.put<Postagem>('https://pessoalbloggonzales.herokuapp.com/postagens', postagem, this.token)
+    }
+
+    deletePostagem(id: number){
+      return this.http.delete(`https://pessoalbloggonzales.herokuapp.com/postagens/${id}`, this.token)
     }
 }
